55128f20d9eee5b865339b5e5c32e01a1de2da54
John Joseph Tanilong <your_email@example.com>
2025-10-15 17:05:09 +0800

    full version

--- src/main/java/com/example/shoppingcartapp/shoppingcart/ShoppingCartService.java
+++ src/main/java/com/example/shoppingcartapp/shoppingcart/ShoppingCartService.java
@@ -1,7 +1,5 @@
 package com.example.shoppingcartapp.shoppingcart;
 
-import com.example.shoppingcartapp.shoppingcart.Product;
-import com.example.shoppingcartapp.shoppingcart.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
